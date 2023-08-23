289f1ccfa06a06a532d64b176cdb5bd261bf1157
Ordana <astralordana@gmail.com>
2023-08-23 00:20:16 -0500

    1.20.1

--- common/src/main/java/com/ordana/experienced_crops/ModTags.java
+++ common/src/main/java/com/ordana/experienced_crops/ModTags.java
@@ -1,6 +1,6 @@
 package com.ordana.experienced_crops;
 
-import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
 
@@ -8,6 +8,6 @@ public class ModTags {
     public static final TagKey<Block> CROPS_WITHOUT_EXP = registerBlockTag("crops_without_exp");
 
     private static TagKey<Block> registerBlockTag(String id) {
-        return TagKey.create(Registry.BLOCK_REGISTRY, ExperiencedCrops.res(id));
+        return TagKey.create(Registries.BLOCK, ExperiencedCrops.res(id));
     }
 }
